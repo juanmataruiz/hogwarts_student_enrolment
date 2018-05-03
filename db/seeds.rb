53a334be2ed22c7b9b82176836ee8682f632d214
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-03 11:14:41 +0100

    Added a student table, save method, and ruby seed file

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require_relative('../models/hogwarts_student')
+
+student1 = HogwartsStudent.new({
+  "first_name" => "Harry"
+  "last_name" => "Potter"
+  "house" => "Gryffindor"
+  "age" => 11
+  })
+
+  student2 = HogwartsStudent.new({
+    "first_name" => "Luna"
+    "last_name" => "Lovegood"
+    "house" => "Ravenclaw"
+    "age" => 12
+    })
+
+student1.save()
+student2.save()
